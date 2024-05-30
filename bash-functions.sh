402eb56896df2f4175581a4bb29a0e83dc6a593a
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-05-30 17:48:33 -0500

    [nc] initial pipeline test
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- bash-functions.sh
+++ bash-functions.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 set -eo pipefail
+
+# awsAssumeRole ()  ================================================================================
+#
+# Assume AWS role.
+# Expects parameters
+# $1 = aws accounts id
+# $2 = aws role to assume
+# Expects IAM credentials to be defined as ENV variables
+
+awsAssumeRole () {
+    aws sts assume-role --output json --role-arn arn:aws:iam::"$1":role/"$2" --role-session-name aws-assume-role > credentials
+
+    export AWS_ACCESS_KEY_ID=$(cat credentials | jq -r ".Credentials.AccessKeyId")
+    export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAccessKey")
+    export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
+}
+
 # createOIDCAssumableRole ()  ================================================================================
 #
 # Kubernetes ServiceAccount role for interacting with AWS API
