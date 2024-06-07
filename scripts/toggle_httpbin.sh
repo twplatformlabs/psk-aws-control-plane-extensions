c0f752f3133662ff757e04ad2168f0dd0c21aa2c
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-06-06 21:59:00 -0500

    [nc] add pdb
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/toggle_httpbin.sh
+++ scripts/toggle_httpbin.sh
@@ -30,12 +30,15 @@ if [[ $toggle == "on" ]]; then
   echo "deploy httpbin to default-mtls"
 
   kubectl apply -f test/httpbin --recursive
+  sleep 180
+
+  # this doesn't work, not sure why
   # if only the management node is running then pause for a karpenter-node to spin up
   nodeCount=$(kubectl get nodes -l kubernetes.io/arch=amd64 | tail -n +2 | wc -l | xargs)
-  if [[ $nodeCount == "1" ]]; then
-    echo "cluster scaled to zero nodes, wiating for instance to come up"
-    sleep 180
-  fi
+  # if [[ $nodeCount == "1" ]]; then
+  #   echo "cluster scaled to zero nodes, wiating for instance to come up"
+  #   sleep 180
+  # fi
 fi
 
 if [[ $toggle == "off" ]]; then
