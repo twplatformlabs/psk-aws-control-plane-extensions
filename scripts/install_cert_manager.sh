3316cd0bcacacd7131a0fe78674ea9241369af31
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-01-05 16:23:59 -0600

    [nc] use instance naming for all values.yaml and test cert-manager 15
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/install_cert_manager.sh
+++ scripts/install_cert_manager.sh
@@ -5,7 +5,7 @@ source bash-functions.sh
 cluster_name=$1
 chart_version=$(jq -er .cert_manager_chart_version $cluster_name.auto.tfvars.json)
 AWS_ACCOUNT_ID=$(jq -er .aws_account_id $cluster_name.auto.tfvars.json)
-AWS_ASSUME_ROLE=$(jq -er .aws_assume_role $cluster_name.auto.tfvars.json)
+#AWS_ASSUME_ROLE=$(jq -er .aws_assume_role $cluster_name.auto.tfvars.json)
 
 echo "cert-manager chart version $chart_version"
 helm repo add jetstack https://charts.jetstack.io --force-update
@@ -18,4 +18,4 @@ helm upgrade --install cert-manager jetstack/cert-manager \
              --version v$chart_version \
              --namespace cert-manager --create-namespace \
              --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"=arn:aws:iam::${AWS_ACCOUNT_ID}:role/PSKRoles/${cluster_name}-cert-manager-sa \
-             --values cert-manager/default-values.yaml
+             --values cert-manager-values/$cluster_name-values.yaml
