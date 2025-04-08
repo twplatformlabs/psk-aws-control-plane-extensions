194fdfac24646abf79db8488d62afbf655bf2768
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-04-08 16:37:25 -0500

    [nc] add details to service e2e testing
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/services_functional_test.sh
+++ scripts/services_functional_test.sh
@@ -7,6 +7,7 @@ export cluster_name=$1
 bash scripts/toggle_httpbin.sh on $cluster_name
 
 # all istio, external-dns, and cert-manager extensions must be healthy for this response to succeed
+echo "test successful ingress, certs, dns management"
 jsonResponse=$(curl -X GET "https://httpbin.$cluster_name.twplatformlabs.org/json" -H "accept: application/json")
 echo "response $jsonResponse"
 if [[ ! $jsonResponse =~ "slideshow" ]]; then
@@ -16,6 +17,8 @@ fi
 
 # ingress is configured to require TLS1.3 or higher, let's include a test for that
 output=$(curl -Iiv --tlsv1.1 "https://httpbin.$cluster_name.twplatformlabs.org" 2>&1)
+echo "testing TLSv1.3 uplevel with 1.1 communication attempt"
+echo $output | grep "SSL connection using TLSv1.3"
 if [[ ! $output =~ "SSL connection using TLSv1.3" ]]; then
   echo "TLSv1.3 not enforced"
   exit 1
